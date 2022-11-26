9119c9f2f7102d383e0ad7d57806e7f6794c4162
alexpacheli <alex@pacheli.com.br>
2022-11-25 23:18:37 -0300

    Inserido Activity Indicator para carregar filmes

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
 import React, {useEffect, useState} from "react";
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 
 import api from "./src/services/api";
 import Filmes from "./src/Filmes";
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes');
       setFilmes(response.data);
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
+  if (loading) {
 
-  return (
-    <View style={styles.container}>
-      <FlatList 
-        data={filmes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({item}) => <Filmes data={item} /> }
-      />
-    </View>
-  );
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <ActivityIndicator color="#121212" size={45} />
+        <Text>Carreganco filmes...</Text>
+      </View>
+    );
+
+  } else {
+
+    return (
+      <View style={styles.container}>
+        <FlatList 
+          data={filmes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => <Filmes data={item} /> }
+        />
+      </View>
+    );
+
+  }
 }
 
 const styles = StyleSheet.create({
