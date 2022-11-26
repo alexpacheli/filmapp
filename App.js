dd6ba203bf8a55aeb3f7980cad09797643edac53
alexpacheli <alex@pacheli.com.br>
2022-11-25 21:18:13 -0300

    Adicionado componente Filmes, axios api, FlatList e useEffect

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from "react";
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+import api from "./src/services/api";
+import Filmes from "./src/Filmes";
 
 export default function App() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes');
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, []);
+
+
   return (
-    <View>
-      <Text>Oi</Text>
+    <View style={styles.container}>
+      <FlatList 
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => <Filmes /> }
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -14,3 +33,8 @@ const styles = StyleSheet.create({
     flex: 1,
   }
 })
+
+/*
+-Json Web Viewer
+-npm install axios
+*/
